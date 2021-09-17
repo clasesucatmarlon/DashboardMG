8d109b9b5b4a13ce8ce32b57e3e53109b04cc3e6
Marlon García <clasesucatmarlon@gmail.com>
2021-09-16 19:57:50 -0500

    Trabajando con componente sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import { Group, ShoppingBasket, ExitToApp } from "@material-ui/icons";
+
 export const Sidebar = () => {
   return (
     <div className="sidebar">
-      <h2>Desde Sidebar</h2>
+      <h2>Sidebar</h2>
+      <ul>
+        <li><span><Group /></span>Users</li>
+        <li><span><ShoppingBasket /></span>Products</li>
+        <li><span><ExitToApp /></span>Logout</li>
+      </ul>
     </div>
   )
 }
